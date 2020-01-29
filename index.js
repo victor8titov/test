8b89f9babcb39dbe7b275fbe4827fb42a0458ad4
victor <victor8titov@yandex.ru>
2020-01-29 18:53:52 +0300

    \# 44 last

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ window.onload = function() {
     this.alert('hellow man.Watch app');
     console.log('run')
    j
-
+s
 
     this.console.log(window.navigator);
     this.console.log(window.navigator);
