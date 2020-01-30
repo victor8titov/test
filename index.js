c7e159d21149daeee09e8b1e04a2e351f4d03c1c
victor <victor8titov@yandex.ru>
2020-01-30 09:36:44 +0300

    ss

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ window.onload = function() {
     this.alert('hellow man.Watch app');
     console.log('run')
    j
-sslkfsdj
 
     this.console.log(window.navigator);
     this.console.log(window.navigator);
